640d64de0ae92482b1d66a2fdc1ba9f51ba0950c
Daniel3dartist <danielsobrinho3dartist@gmail.com>
2025-08-24 22:50:12 -0300

    Added GPT Model

--- book_exercise/ch/4/dummy_gpy_model.py
+++ book_exercise/ch/4/dummy_gpy_model.py
@@ -1,6 +1,8 @@
 import torch
 from torch import nn
 
+from multiheadattention import MultiHeadAttention
+
 GPT_CONFG_124M = {
     "vocab_size": 50257,    # Vocabulary size
     "context_length": 1024, # Context length
@@ -78,6 +80,41 @@ class GELU(nn.Module):
         ))
 
 
+class ExampleDeepNeuralNetwork(nn.Module):
+    def __init__(self, layer_sizes, use_shortcut):
+        super().__init__()
+        self.use_shortcut = use_shortcut
+        self.layers = nn.ModuleList([
+            nn.Sequential(nn.Linear(layer_sizes[0], layer_sizes[1], GELU())),
+            nn.Sequential(nn.Linear(layer_sizes[1], layer_sizes[2], GELU())),
+            nn.Sequential(nn.Linear(layer_sizes[2], layer_sizes[3], GELU())),
+            nn.Sequential(nn.Linear(layer_sizes[3], layer_sizes[4], GELU())),
+            nn.Sequential(nn.Linear(layer_sizes[4], layer_sizes[5], GELU()))
+        ])
+    
+    def forward(self, x):
+        for layer in self.layers:
+            layer_output = layer(x)
+            if self.use_shortcut and x.shape == layer_output.shape:
+                x = x + layer_output
+            else:
+                x = layer_output
+        return x
+
+def print_gradients(model, x):
+    output = model(x)
+    target = torch.tensor([[0.]])
+
+    loss = nn.MSELoss()
+    loss = loss(output, target)
+
+    loss.backward()
+
+    for name, param in model.named_parameters():
+        if 'weight' in name:
+            print(f"{name} has gradient mean of {param.grad.abs().mean().item()}")
+
+
 class FeedForward(nn.Module):
     def __init__(self, cfg):
         super().__init__()
@@ -89,6 +126,39 @@ class FeedForward(nn.Module):
     
     def forward(self, x):
         return self.layers(x)
+
+
+class TransformerBlock(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.att = MultiHeadAttention(
+            d_in=cfg["emb_dim"],
+            d_out=cfg["emb_dim"],
+            context_length=cfg["context_length"],
+            num_heads=cfg["n_heads"],
+            dropout=cfg["drop_rate"],
+            qkv_bias=cfg["qkv_bias"]
+        )
+        self.ff = FeedForward(cfg)
+        self.norm1 = LayerNorm(cfg["emb_dim"])
+        self.norm2 = LayerNorm(cfg["emb_dim"])
+        self.drop_shortcut = nn.Dropout(cfg["drop_rate"])
+
+    def forward(self, x):
+        shortcut = x
+        x = self.norm1(x)
+        x = self.att(x)
+        x = self.drop_shortcut(x)
+        x = x + shortcut
+
+        shortcut = x
+        x = self.norm2(x)
+        x = self.ff(x)
+        x = self.drop_shortcut(x)
+        x = x + shortcut
+
+        return x
+
 
 if __name__ == "__main__":
     import tiktoken
@@ -180,3 +250,40 @@ if __name__ == "__main__":
     out = ffn(x)
     print(out.shape)
     
+    print('='*80)
+
+    from importlib.metadata import version
+
+    print("matplotlib version:", version("matplotlib"))
+    print("torch version:", version("torch"))
+    print("tiktoken version:", version("tiktoken"))
+
+    print('='*80)
+
+    layer_sizes = [3, 3, 3, 3, 3, 1]
+
+    sample_input = torch.tensor([[1.,0.,-1.]])
+
+    torch.manual_seed(123)
+    model_without_shortcut = ExampleDeepNeuralNetwork(
+        layer_sizes, use_shortcut=False
+    )
+    print_gradients(model_without_shortcut, sample_input)
+
+    print('-'*80)
+
+    torch.manual_seed(123)
+    model_without_shortcut = ExampleDeepNeuralNetwork(
+        layer_sizes, use_shortcut=True
+    )
+    print_gradients(model_without_shortcut, sample_input)
+
+    print('='*80)
+
+    torch.manual_seed(123)
+    x = torch.rand(2, 4, 768)
+    block = TransformerBlock(GPT_CONFG_124M)
+    output = block(x)
+
+    print("Input shape: ", x.shape)
+    print("Output shape: ", output.shape)
---
+++ book_exercise/ch/4/gpt_model.py
@@ -0,0 +1,130 @@
+import torch
+from torch import nn
+
+from dummy_gpy_model import TransformerBlock, \
+                            LayerNorm, \
+                            GPT_CONFG_124M
+
+class GPTModel(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+
+        self.tok_emb = nn.Embedding(cfg["vocab_size"], cfg["emb_dim"])
+        self.pos_emb = nn.Embedding(cfg["context_length"], cfg["emb_dim"])
+        self.drop_emb = nn.Dropout(cfg["drop_rate"])
+
+        self.trf_blocks = nn.Sequential(
+            *[TransformerBlock(cfg) for _ in range(cfg["n_layers"])]
+        )
+
+        self.final_norm = LayerNorm(cfg["emb_dim"])
+        self.out_head = nn.Linear(
+            cfg["emb_dim"], cfg["vocab_size"], bias=False
+        )
+
+    def forward(self, in_idx):
+        batch_size, seq_len = in_idx.shape
+        tok_embeds = self.tok_emb(in_idx)
+        pos_embeds = self.pos_emb(torch.arange(seq_len, device=in_idx.device))
+        x = tok_embeds + pos_embeds
+        x = self.drop_emb(x)
+        x = self.trf_blocks(x)
+        x = self.final_norm(x)
+
+        logits = self.out_head(x)
+        return logits
+    
+
+def generate_text_simple(model, idx, max_new_tokens, context_size):
+    for _ in range(max_new_tokens):
+        idx_cond = idx[:, -context_size:]
+
+        with torch.no_grad():
+            logits = model(idx_cond)
+
+        logits = logits[:, -1, :]
+        
+        probas = torch.softmax(logits, dim=-1)
+
+        idx_next = torch.argmax(probas, dim=-1, keepdim=True)
+
+        idx = torch.cat((idx, idx_next), dim=1)
+
+    return idx
+
+if __name__ == "__main__":
+    import tiktoken
+
+    tokenizer = tiktoken.get_encoding("gpt2")
+
+    batch = []
+
+    txt1 = "Every effort moves you"
+    txt2 = "Every day holds a"
+
+    batch.append(torch.tensor(tokenizer.encode(txt1)))
+    batch.append(torch.tensor(tokenizer.encode(txt2)))
+    batch = torch.stack(batch, dim=0)
+    print(batch)
+
+
+    #torch.manual_seed(123)
+
+    model = GPTModel(GPT_CONFG_124M)
+
+    out = model(batch)
+    print("Input batch:\n", batch)
+    print("\nOutput shape: ", out.shape)
+    print(out)
+    
+    print('='*80)
+
+    total_params = sum(p.numel() for p in model.parameters())
+    print(f"Total number of parameters: {total_params:,}")
+
+    print('='*80)
+
+    print("Token embedding layer shape:", model.tok_emb.weight.shape)
+    print("Output layer shape:", model.out_head.weight.shape)
+
+    print('='*80)
+
+    total_params_gpt2 =  total_params - sum(p.numel() for p in model.out_head.parameters())
+    print(f"Number of trainable parameters considering weight tying: {total_params_gpt2:,}")
+
+    print('='*80)
+
+    total_size_bytes = total_params * 4
+    total_size_mb = total_size_bytes / (1024 * 1024)
+
+    print(f"Total size of the model: {total_size_mb:.2f} MB")
+
+    print('='*80)
+
+    start_context = "Hello, I am"
+
+    encoded = tokenizer.encode(start_context)
+    print("encoded: ", encoded)
+
+    encoded_tensor = torch.tensor(encoded).unsqueeze(0)
+    print("encoded_tensor.shape: ", encoded_tensor.shape)
+
+    print('='*80)
+
+    model.eval()
+
+    out = generate_text_simple(
+        model=model,
+        idx=encoded_tensor,
+        max_new_tokens=6,
+        context_size=GPT_CONFG_124M["context_length"]
+    )
+
+    print("Output: ", out)
+    print("Output length: ", len(out[0]))
+
+    print('='*80)
+
+    decoded_text = tokenizer.decode(out.squeeze(0).tolist())
+    print(decoded_text)
+    print('='*80)
